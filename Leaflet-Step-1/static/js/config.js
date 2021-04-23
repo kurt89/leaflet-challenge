d190a2ad412ea50251d4ce2ee20569262f39a5e0
Kurt <kurt.dietrich.85@gmail.com>
2021-04-22 20:42:41 -0400

    3rd and final commit

--- Leaflet-Step-1/static/js/config.js
+++ Leaflet-Step-1/static/js/config.js
@@ -1,2 +1,6 @@
-// API key
-const API_KEY = "YOUR API KEY HERE!";
+//API key
+var config = {
+
+    API_KEY:'pk.eyJ1Ijoia3VydDg5IiwiYSI6ImNrbmkxOHk1YjB2ZXAyb280cTV0NDdxbWQifQ.z6QNvW-7GbHOHyyByHcI5Q'
+
+}
